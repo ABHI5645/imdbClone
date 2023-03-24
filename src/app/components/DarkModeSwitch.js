85eae68db77b47df5497e73991561e831996f8b9
Abhi <abhi24.8896@gmail.com>
2023-03-24 21:36:02 +0530

    headerAndDarkMode

---
+++ src/app/components/DarkModeSwitch.js
@@ -0,0 +1,25 @@
+"use client"
+import React, { useEffect, useState } from 'react'
+import {MdOutlineLightMode} from 'react-icons/md'
+import {MdDarkMode} from 'react-icons/md'
+import { useTheme } from 'next-themes'
+export default function DarkModeSwitch() {
+  const {systemTheme,theme,setTheme}=useTheme();
+  const [mounted,setMounted]=useState(false);
+  useEffect(()=>setMounted(true),[]);
+  const currentTheme =theme==='system'?systemTheme:theme;
+  return (
+    <>
+
+    {mounted && (currentTheme==='dark'?(
+    
+        <MdOutlineLightMode className='text-xl cursor-pointer hover:text-amber-500' onClick={()=>setTheme('light')}/>
+    ):(
+        <MdDarkMode className='text-xl cursor-pointer hover:text-amber-500' onClick={()=>setTheme('dark')}/>
+    ))
+   }
+    
+    </>
+
+  )
+}
