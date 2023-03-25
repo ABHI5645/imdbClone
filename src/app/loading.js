3ed929bf904a3628563a401a5e5dd527904daf60
Abhi <abhi24.8896@gmail.com>
2023-03-25 16:25:24 +0530

    loading

---
+++ src/app/loading.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function loading() {
+  return (
+    <div className='flex justify-center'>
+      <img src="h-96 spinner.svg" alt="loading..."/>
+    </div>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,18 @@ const API_KEY=process.env.API_KEY;
 import Results from "./components/Results";
 export default async function Home({searchParams}) {
   const genre=searchParams.genre || "fetchTrending";
-  const res=await fetch(`https://api.themoviedb.org/3/${genre === 'fetchTopRated'?'movie/top_rated':'trending/all/week'}?api_key=${API_KEY}&language=en-US&page=1`,{next:{revalidate:10}});
+ 
+    const res=await fetch(`https://api.themoviedb.org/3/${genre === 'fetchTopRated'?'movie/top_rated':'trending/all/week'}?api_key=${API_KEY}&language=en-US&page=1`,{next:{revalidate:10}});
+  
+ 
   if(!res.ok){
     throw new Error('Failed to fetch data')
   }
   const data=await res.json();
-  const results=data.results;
+ 
+    const results=data.results;
+  
+  
   console.log(results);
   return (
     <div>
