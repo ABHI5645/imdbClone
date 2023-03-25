db11334c4994b7e50a0de563d53e32ca7d4f33be
Abhi <abhi24.8896@gmail.com>
2023-03-25 18:09:28 +0530

    movie page

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Results from "./components/Results";
 export default async function Home({searchParams}) {
   const genre=searchParams.genre || "fetchTrending";
  
-    const res=await fetch(`https://api.themoviedb.org/3/${genre === 'fetchTopRated'?'movie/top_rated':'trending/all/week'}?api_key=${API_KEY}&language=en-US&page=1`,{next:{revalidate:10}});
+    const res=await fetch(`https://api.themoviedb.org/3/${genre === 'fetchTopRated'?'movie/top_rated':'trending/all/week'}?api_key=${API_KEY}&language=en-US&page=1`,{next:{revalidate:1000}});
   
  
   if(!res.ok){
